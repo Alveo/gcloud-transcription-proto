4f59de7b878591f6f41c3a295aaff572e362cd05
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-11-11 16:01:04 +1100

    Add support for audio longer than 60 seconds

--- main.py
+++ main.py
@@ -9,43 +9,77 @@ from google.cloud import speech
 from google.cloud.speech import enums
 from google.cloud.speech import types
 
-def gcloud_transcribe_short(params):
+speech_client = speech.SpeechClient()
+storage_client = google.cloud.storage.Client()
+
+def gcloud_transcribe_short(config):
     """
     Uses Google Cloud Speech-to-Text to transcribe audio files up 
     to durations of 60 seconds.
 
-    @params: Provided as a dict, requires at minimum:
+    @config: Provided as a dict, requires at minimum:
         - filepath - path to file
         - sample_rate_hertz - gcloud setting, typically 16000
         - language_code - gcloud setting, e.g 'en-AU' or 'ja-JP'
         - encoding - gcloud setting, e.g 
                         enums.RecognitionConfig.AudioEncoding.LINEAR16
 
-        Other examples of params that might be of use:
+        Other examples of config options that might be of use:
         - enable_speaker_diarization=False, diarization_speaker_count=2):
         - enable_word_time_offsets=False
 
     @return: Response object from Google Cloud Speech
     """
-    if params['filepath'] is None:
-        raise Exception("`filepath` not specified for segmentation operation.")
-
-    client = speech.SpeechClient()
+    try:
+        file_path = config.pop('filepath')
+    except KeyError:
+        raise Exception("`filepath` not specified for transcription operation.")
 
     # Read file into memory before uploading
-    with io.open(params['filepath'], 'rb') as audio_file:
+    with io.open(file_path, 'rb') as audio_file:
         content = audio_file.read()
         audio = types.RecognitionAudio(content=content)
     
-    # Filter non-Google Cloud parameters
-    params.pop('filepath')
-
-    config = types.RecognitionConfig(
-        **{k: v for k, v in params.items() if v is not None}
-    )
-
     # Detects speech in the audio file
-    return client.recognize(config, audio)
+    return speech_client.recognize(config, audio)
+
+def gcloud_transcribe_long(config):
+    """
+    Uses Google Cloud Speech-to-Text to transcribe audio files beyond
+    a duration of 60 seconds. Note that files must be uploaded to Google-
+    -Cloud Storage (GCS) before this can be ran. See `gcloud_upload_file`
+    for more information.
+
+    @config: Provided as a dict, requires at minimum:
+        - remotepath - path to gs://bucket_name/file_path
+        - sample_rate_hertz - gcloud setting, typically 16000
+        - language_code - gcloud setting, e.g 'en-AU' or 'ja-JP'
+        - encoding - gcloud setting, e.g 
+                        enums.RecognitionConfig.AudioEncoding.LINEAR16
+        - timeout - how long to wait before timing out
+
+        Other examples of config options that might be of use:
+        - enable_speaker_diarization=False, diarization_speaker_count=2):
+        - enable_word_time_offsets=False
+
+    @return: Response object from Google Cloud Speech
+    """
+    try:
+        remote_path = config.pop('remotepath')
+    except KeyError:
+        raise Exception("`remotepath` not specified for transcription operation.")
+
+    try:
+        timeout = config.pop('timeout')
+    except KeyError:
+        raise Exception("`timeout` not specified for transcription operation.")
+
+    audio = types.RecognitionAudio(uri=remote_path)
+    operation = speech_client.long_running_recognize(config, audio)
+
+    response = operation.result(timeout=timeout)
+
+    return result
 
 def gcloud_upload_file(local_filepath, gcloud_bucket_name):
     """
@@ -57,7 +91,6 @@ def gcloud_upload_file(local_filepath, gcloud_bucket_name):
 
     @return: remote url of the uploaded file
     """
-    storage_client = google.cloud.storage.Client()
     bucket = storage_client.get_bucket(gcloud_bucket_name)
     remote_filepath = "%s__%s" % (
                 os.path.basename(local_filepath),
@@ -65,8 +98,6 @@ def gcloud_upload_file(local_filepath, gcloud_bucket_name):
             )
 
     blob = bucket.blob(remote_filepath)
-    print("Uploading `%s` as `%s`..." % 
-            (local_filepath, remote_filepath))
 
     # Read the file and upload it
     with io.open(local_filepath, 'rb') as file:
@@ -81,10 +112,12 @@ def gcloud_upload_file(local_filepath, gcloud_bucket_name):
     return url
 
 params = {
-    'filepath': sys.argv[1],
+    #'filepath': sys.argv[1],
+    'remotepath': 'gs://alveo-transcriber/audio.wav__f3d4e33a-8aac-4562-9247-11495ebcce25',
     'sample_rate_hertz': 16000,
     'language_code': 'en-AU',
     'encoding': enums.RecognitionConfig.AudioEncoding.LINEAR16
 }
 #gcloud_upload_file(sys.argv[1], 'alveo-transcriber')
-print(auto_transcribe_short(params))
+#print(gcloud_transcribe_short(params))
+print(gcloud_transcribe_long(params))
